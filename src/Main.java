d6b6b5288b899ad7500ce46296b7c4d833ebd30f
samuel Galliani-Royer <samuel.galliani-royer@edu.esiee-it.fr>
2024-10-08 10:09:05 +0200

    fonction TP2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /**
+     * Renvoie boolean si une valeur du tableau<10
+     * @param tab a tab
+     * @return boolean if i<10
+     **/
+    static boolean superieur(int [] tab ){
+        for (int i=0; i<tab.length; i++){
+            if (tab[i] < 10){
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Renvoie la valeur max du tableau
+     * @param tab un tableau
+     * @return valeur max
+     **/
+    static int maximum(int [] tab){
+        int max= 0;
+        for (int i=0; i<tab.length; i++){
+            if (tab[i]>max){
+                max=tab[i];
+            }
+        }
+        return max;
+    }
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //System.out.printf("Hello and welcome!");
+    int cpt =0;
+    int [] score=new int[7];
+    for (int i = 0; i < score.length; i++) {
+        score[i]= i;
+        cpt+=i;
+        System.out.print(score[i]+ " ");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    }
+    System.out.println("\n" + "Moyenne = " + cpt/score.length);
+    System.out.println(maximum(score));
+    System.out.println(superieur(score));
     }
 }
