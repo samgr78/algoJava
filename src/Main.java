f2e076cefe175f78c6220ce22b92c14c10ea6def
samuel Galliani-Royer <samuel.galliani-royer@edu.esiee-it.fr>
2024-10-08 14:54:01 +0200

    recherche binaire2

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,28 @@ public class Main {
      * @param a un entier a trouver dans le tab
      * @return l'index de l'entier ou -1 si l'entier n'est pas dans le tab
      */
-    static int rechercheBinaire(int [] tab,int  a){
+    static int rechercheLineaire(int [] tab,int  a){
         for (int i=0; i<tab.length; i++){
             if (tab[i]==a){
                 return i;
+            }
+        }
+        return -1;
+    }
+    static int rechercheBinaire(int [] tab, int a) {
+        int min = 0;
+        int max = tab.length-1;
+        int mid;
+        while (min <= max) {
+            mid = (min+max) / 2;
+            if (a == tab[mid]) {
+                return mid;
+            }
+            if (a < tab[mid]) {
+                max = mid -1;
+            }
+            else {
+                min = mid+1;
             }
         }
         return -1;
@@ -57,5 +75,16 @@ public class Main {
     System.out.println("\n" + "Moyenne = " + cpt/score.length);
     System.out.println(maximum(score));
     System.out.println(superieur(score));
+    System.out.println(score);
+    int [] abc=new int [7];
+    abc[0]=2;
+    abc[1]=3;
+    abc[2]=5;
+    abc[3]=7;
+    abc[4]=8;
+    abc[5]=9;
+    abc[6]=10;
+    System.out.println(rechercheLineaire(abc, 4));
+    System.out.println(rechercheBinaire(abc, 5));
     }
 }
